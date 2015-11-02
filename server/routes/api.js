0645d5982c6a85b112a88823d31d53be8688bcb5
brandon932 <bthornton_932@yahoo.com>
2015-11-02 11:24:52 -0700

    added restfull routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Talks = require("../models/petes.js");
+var Talk = require("../models/petes.js");
 
 router.get('/talks', function(req, res, next) {
-    Talks.find(function(err, talk){
+    Talk.find(function(err, talk){
         if (err) {
             res.json({"message": err});
         }else{
@@ -14,9 +14,41 @@ router.get('/talks', function(req, res, next) {
 router.post('/talks', function(req,res,next){
     newTalk = new Talk(req.body);
     newTalk.save(function(err, talk){
-        
+        if (err) {
+            res.json({'message': err});
+        }else{
+            res.json(talk);
+        }
     });
+});
+router.get('/talk/:id', function(req,res,next){
+    Talk.findById(req.params.id, function(err, talk){
+        if (err) {
+            res.json({'message': err});
+        }else{
+            res.json(talk);
+        }
+    });
+});
 
+router.put('/talk/:id', function(req,res,next){
+    var options = {"new":true};
+    Talk.findByIdAndUpdate(req.params.id, req.body, options, function(err, talk){
+        if (err) {
+            res.json({'message': err});
+        }else{
+            res.json(talk);
+        }
+    });
+});
+router.delete('/talk/:id', function(req,res,next){
+    Talk.findByIdAndRemove(req.params.id, function(err, talk){
+        if (err) {
+            res.json({'message': err});
+        }else{
+            res.json({'message': "success!"});
+        }
+    });
 });
 
 
